95c7ca8e9ffb99ea35172535c65ef8c823844129
skim-93 <68123310+skim-93@users.noreply.github.com>
2021-08-07 13:58:23 -0700

    Time: 6 ms (53.14%), Space: 44.9 MB (54.37%) - LeetHub

---
+++ contains-duplicate/contains-duplicate.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        /*
+        //Brute-Force Solution
+        for(int i = 0; i < nums.length-1; i++) {
+            for(int j = i+1; j < nums.length; j++) {
+                if(nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        //Time Complexity: O(n^2)
+        //Space Complexity: O(1)
+        //Time limit Exceeded
+        */
+        
+        //Optimized solution - Use of Hashset to store and use contains
+        
+        Set<Integer> set = new HashSet<Integer>();
+        for(int num : nums) {
+            if(set.contains(num)) { 
+                return true;
+            }
+            set.add(num);
+        }
+    
+        return false;
+    }
+}
