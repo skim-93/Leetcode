2c413461f389f3a0910f7f8d83fe5b094cf97e41
skim-93 <68123310+skim-93@users.noreply.github.com>
2021-08-20 19:29:58 -0700

    Time: 3 ms (61.31%), Space: 39.1 MB (76.99%) - LeetHub

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -1,42 +1,23 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        /*
-        //Brute-Force Solution
-        for(int i = 0; i < nums.length-1; i++) {
-            for(int j = i+1; j < nums.length; j++) {
-                if(nums[i]+nums[j] == target) {
-                    int[] ans = new int[2];
-                    ans[0] = i;
-                    ans[1] = j;
-                    return ans;
-                }
-            }
-        }
-        //time Complexity = O(n^2);
-        //Space Complexity = O(1);
-        return null;
-        */
+        // solve it with extra space to save the difference.
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         
-        //Optimized Solution - use of Additional Space (Hash Map)
-        
-        Map<Integer,Integer> map = new HashMap<Integer, Integer>();
-        int counter = 0;
-        
-        for(int num : nums) {
-            int numDiff = target - num;
-            if(map.containsKey(numDiff)) {
+        for(int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if(map.containsKey(diff)) {
                 int[] ans = new int[2];
-                ans[0] = map.get(numDiff);
-                ans[1] = counter;
+                ans[0] = map.get(diff);
+                ans[1] = i;
                 return ans;
-            } else {
-                map.put(num, counter);
             }
-            counter++;
+            map.put(nums[i], i);
+            
+            
         }
-        //time Complexity = O(n)
-        //space Complexity = O(n)
         
-        return null;
+        
+        return new int[]{-1,-1};
+    
     }
 }
