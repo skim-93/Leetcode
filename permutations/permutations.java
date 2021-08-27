1210bac66a0b199e52c97322b8e040897daa646d
skim-93 <68123310+skim-93@users.noreply.github.com>
2021-08-27 16:08:15 -0700

    Time: 1 ms (92.92%), Space: 39.1 MB (80.27%) - LeetHub

---
+++ permutations/permutations.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        boolean[] visited = new boolean[nums.length];
+        dfs(res, nums, new ArrayList(), visited);
+        return res;
+    }
+    
+    public void dfs(List<List<Integer>> res, int[] nums, List<Integer> curr, boolean[] visited) {
+        if(curr.size() == nums.length) {
+            res.add(new ArrayList(curr));
+            return; 
+        }
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(visited[i] == true) {
+                continue;
+            }
+            
+            curr.add(nums[i]);
+            visited[i] = true;
+            dfs(res, nums, curr, visited);
+            curr.remove(curr.size() - 1);
+            visited[i] = false;
+            
+        }
+    }
+}
