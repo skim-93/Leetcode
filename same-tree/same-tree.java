1ed9e95a0bd03219bd9bc3f4c57dadb8de8f92d7
skim-93 <68123310+skim-93@users.noreply.github.com>
2021-09-08 23:06:47 -0700

    Time: 0 ms (100.00%), Space: 38.5 MB (7.94%) - LeetHub

--- same-tree/same-tree.java
+++ same-tree/same-tree.java
@@ -23,9 +23,6 @@ class Solution {
         if(p == null || q == null) return false;
         if(p.val != q.val) return false;
         
-
-        
         return isSame(p.left, q.left) && isSame(p.right, q.right);
-        
     }
 }
