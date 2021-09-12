64913ecca7737a9d50ec7f9896a6e89b3eae6dc5
skim-93 <68123310+skim-93@users.noreply.github.com>
2021-09-12 16:01:48 -0700

    Time: 1 ms (93.72%), Space: 39.1 MB (73.06%) - LeetHub

--- permutations/permutations.java
+++ permutations/permutations.java
@@ -1,17 +1,22 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
+        
+        //this to track, if we visited, certain index
         boolean[] visited = new boolean[nums.length];
+        
         dfs(res, nums, new ArrayList(), visited);
         return res;
     }
     
     public void dfs(List<List<Integer>> res, int[] nums, List<Integer> curr, boolean[] visited) {
+        //if currentsize reaches nums.length, that means ,we found the permutation successfully
         if(curr.size() == nums.length) {
             res.add(new ArrayList(curr));
             return;
         }
         
+        //for loop to go through each index, 
         for(int i = 0; i < nums.length; i++) {
             if(visited[i] == true) {
                 continue;
