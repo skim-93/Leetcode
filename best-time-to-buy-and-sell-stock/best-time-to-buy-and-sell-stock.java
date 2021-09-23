cd7e1fe59ec3b134aae36dd0a15f7a51d1517d29
skim-93 <68123310+skim-93@users.noreply.github.com>
2021-09-23 16:41:26 -0700

    Time: 1 ms (100.00%), Space: 51.9 MB (88.65%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,37 +1,18 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        //Initial thought was to find bigest profit front to back, then store those Max profit as bruteforce
-        /*
-        int maxPro = 0;
-        
-        for(int i = 0; i < prices.length-1; i++) {
-            for(int j = i+1; j < prices.length; j++) {
-                maxPro = Math.max(maxPro, prices[j] - prices[i]);
-            }
-        }
-        */
-        //Time Complexity: O(n^2)
-        //Space Complexity: O(1)
-        //I got time out error when using this method, which I need optimized solution
-        
-        //noticed that we only need to change min number when new min number comes out, and when new max number is          out else we do not need to do anything to maxPro
-        
-        //optimized solution - using local variable min and max
-        
-        int maxPro = 0;
-        int min = Integer.MAX_VALUE;
+        int minValue = prices[0];
+        int max = 0;
         
         for(int i = 0; i < prices.length; i++) {
-            if(prices[i] < min) {
-                min = prices[i];
-            } else if(prices[i] - min > maxPro) {
-                maxPro = prices[i] - min;
+            if(prices[i] < minValue) {
+                minValue = prices[i];
             }
             
+            else if(max < prices[i] - minValue) {
+                max = prices[i] - minValue;
+            }
         }
-        //Time Complexity O(n)
-        //Space Complexity O(1)
         
-        return maxPro;
+        return max;
     }
 }
