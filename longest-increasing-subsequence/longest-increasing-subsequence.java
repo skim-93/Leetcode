6818ac2ca18baacac86541ec1bd7a18f2993762f
skim-93 <68123310+skim-93@users.noreply.github.com>
2021-09-25 21:56:41 -0700

    Time: 58 ms (52.30%), Space: 38.8 MB (57.39%) - LeetHub

---
+++ longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int max = 1;
+        
+        for(int i = nums.length - 1; i >= 0; i--) {
+            dp[i] = 1;
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[j] > nums[i]) {
+                    dp[i] = Math.max(dp[i], 1 + dp[j]);
+                }
+            }
+            max = Math.max(dp[i], max);
+        }
+        
+        
+        return max;
+    }
+}
