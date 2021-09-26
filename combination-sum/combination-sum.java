541b08c8575385f67b7f6504b795d0aaeadb335c
skim-93 <68123310+skim-93@users.noreply.github.com>
2021-09-25 18:55:13 -0700

    Time: 6 ms (34.33%), Space: 41.6 MB (17.58%) - LeetHub

--- combination-sum/combination-sum.java
+++ combination-sum/combination-sum.java
@@ -1,31 +1,31 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList();
         Arrays.sort(candidates);
-        List<Integer> curr = new ArrayList<>();
-    
-        dfs(res, curr, 0, target, candidates, 0);
+        dfs(candidates, target, res, new ArrayList(), 0, 0);
         return res;
-    }
+    }    
     
-    public void dfs(List<List<Integer>> res, List<Integer> curr, int sum, int target, int[] candidates, int index) {
+    public void dfs(int[] candidates, int target, List<List<Integer>> res, List<Integer> current, int sum,int index) {
         if(sum == target) {
-            if(!res.contains(curr)) {
-                res.add(new ArrayList(curr));
+            if(!res.contains(current)) {
+                res.add(new ArrayList(current));
             }
             return;
         }
+        
+
         
         for(int i = index; i < candidates.length; i++) {
             if(sum + candidates[i] > target) {
                 return;
             }
+            sum += candidates[i];
             
-            curr.add(candidates[i]);
-            sum = sum + candidates[i];
-            dfs(res, curr, sum, target, candidates, i);
-            curr.remove(curr.size() - 1);
-            sum = sum - candidates[i];
+            current.add(candidates[i]);
+            dfs(candidates, target , res, current, sum, i);
+            current.remove(current.size() - 1);
+            sum -= candidates[i];
         }
     }
     
