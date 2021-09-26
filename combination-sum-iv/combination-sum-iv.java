268f739606a9e1993b8642651743a06ff5d4376d
skim-93 <68123310+skim-93@users.noreply.github.com>
2021-09-25 17:38:25 -0700

    Time: 0 ms (100.00%), Space: 36.5 MB (52.42%) - LeetHub

---
+++ combination-sum-iv/combination-sum-iv.java
@@ -0,0 +1,24 @@
+class Solution {
+    
+    public int combinationSum4(int[] nums, int target) {
+        Integer[] memo = new Integer[target + 1];
+        return dfs(nums, target, memo);
+    }
+    
+    public int dfs(int[] nums, int target, Integer[] memo) {
+        if(target == 0) return 1;
+        if(target < 0) return 0;
+        
+        if(memo[target] != null) return memo[target];
+        
+        int total = 0;
+        for(int i = 0; i < nums.length; i ++) {
+            total += dfs(nums, target - nums[i], memo);
+        }
+        
+        memo[target] = total;
+        return total;
+        
+    }
+    
+}
