62e4f6df347d060f5606ab98d23fefbe6403bd4c
skim-93 <68123310+skim-93@users.noreply.github.com>
2021-09-27 12:48:22 -0700

    Time: 6 ms (66.81%), Space: 40.1 MB (60.26%) - LeetHub

--- cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,20 +1,24 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        int[] cost=new int[n];
-        Arrays.fill(cost,Integer.MAX_VALUE);
-        cost[src]=0;
-        for(int i=0;i<=k;i++)
-        {
-            int[] temp= Arrays.copyOf(cost,n);
-            for(int[] f: flights)
-            {
-                int curr=f[0],next=f[1],price=f[2];
-                if(cost[curr]==Integer.MAX_VALUE)
-                    continue;
-                temp[next]=Math.min(temp[next],cost[curr]+price);
+        int[] cost = new int[n];
+        Arrays.fill(cost, Integer.MAX_VALUE);
+        
+        cost[src] = 0;
+        
+        for(int i = 0; i <= k; i++) {
+            int[] temp = Arrays.copyOf(cost, n);
+            for(int[] f : flights) {
+                int current = f[0];
+                int next = f[1];
+                int price = f[2];
+                
+                if(cost[current] != Integer.MAX_VALUE) {
+                    temp[next] = Math.min(temp[next], cost[current] + price);
+                }
             }
-            cost=temp;
+            cost = temp;
         }
-        return cost[dst]==Integer.MAX_VALUE?-1:cost[dst];
+        if(cost[dst] == Integer.MAX_VALUE) return -1;
+        else return cost[dst];
     }
 }
