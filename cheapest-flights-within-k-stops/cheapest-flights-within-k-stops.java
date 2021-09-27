8a436c4b957dc18f87b2589344fbb9a8e29a19b7
skim-93 <68123310+skim-93@users.noreply.github.com>
2021-09-27 12:13:36 -0700

    Time: 6 ms (66.78%), Space: 40.4 MB (51.02%) - LeetHub

--- cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,38 +1,20 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        Map<Integer, List<int[]>> graph = new HashMap<>();
-        Map <Integer, Integer> visited = new HashMap<>(); 
-        
-        for (int[] flight: flights) {
-            graph.putIfAbsent(flight[0], new ArrayList<int[]>());
-            graph.get(flight[0]).add(new int[]{flight[1], flight[2]});
-        }
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-
-        
-        pq.add(new int[] {src, 0, 0});
-        
-        
-         while (!pq.isEmpty()) {
-            int [] arr = pq.poll(); 
-            int curr = arr[0]; 
-            int cost = arr[1]; 
-            int stop = arr[2]; 
-            
-            if (dst == curr) return cost;
-             
-            if (visited.containsKey(curr) && visited.get(curr) < stop) continue; 
-                visited.put(curr, stop); 
-            
-            if (stop > k)  continue;
-             
-            if (!graph.containsKey(curr)) continue; 
-             
-            for (int [] flight : graph.get(curr)) 
-                  pq.offer(new int []{flight[0], cost + flight[1], stop+1});    
+        int[] cost=new int[n];
+        Arrays.fill(cost,Integer.MAX_VALUE);
+        cost[src]=0;
+        for(int i=0;i<=k;i++)
+        {
+            int[] temp= Arrays.copyOf(cost,n);
+            for(int[] f: flights)
+            {
+                int curr=f[0],next=f[1],price=f[2];
+                if(cost[curr]==Integer.MAX_VALUE)
+                    continue;
+                temp[next]=Math.min(temp[next],cost[curr]+price);
             }
-        
-       return -1; 
+            cost=temp;
+        }
+        return cost[dst]==Integer.MAX_VALUE?-1:cost[dst];
     }
 }
