6a9e58c86f1f4d40629a0fc11b0eadf37956a0b9
skim-93 <68123310+skim-93@users.noreply.github.com>
2021-09-27 15:04:16 -0700

    Time: 39 ms (10.70%), Space: 40.9 MB (38.68%) - LeetHub

--- cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,24 +1,30 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        int[] cost = new int[n];
-        Arrays.fill(cost, Integer.MAX_VALUE);
+        Map<Integer, Integer> cost = new HashMap();
+        for(int[] f : flights) {
+            cost.put(f[0], Integer.MAX_VALUE);
+            if(!cost.containsKey(f[1])) {
+                cost.put(f[1], Integer.MAX_VALUE);
+            }
+        }
         
-        cost[src] = 0;
+        cost.put(src, 0);
         
         for(int i = 0; i <= k; i++) {
-            int[] temp = Arrays.copyOf(cost, n);
+            Map<Integer, Integer> temp = new HashMap();
+            temp.putAll(cost);
             for(int[] f : flights) {
                 int current = f[0];
                 int next = f[1];
                 int price = f[2];
                 
-                if(cost[current] != Integer.MAX_VALUE) {
-                    temp[next] = Math.min(temp[next], cost[current] + price);
+                if(cost.get(current) != Integer.MAX_VALUE) {
+                    temp.put(next,  Math.min(temp.get(next), cost.get(current) + price));
                 }
             }
             cost = temp;
         }
-        if(cost[dst] == Integer.MAX_VALUE) return -1;
-        else return cost[dst];
+        if( cost.get(dst) == null || cost.get(dst) == Integer.MAX_VALUE) return -1;
+        else return cost.get(dst);
     }
 }
