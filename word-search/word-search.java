a3007bef81731f92ba73079aacf416ee5bb3d67f
skim-93 <68123310+skim-93@users.noreply.github.com>
2021-09-26 20:12:30 -0700

    Time: 90 ms (87.00%), Space: 37.2 MB (68.27%) - LeetHub

--- word-search/word-search.java
+++ word-search/word-search.java
@@ -1,38 +1,37 @@
 class Solution {
-    public boolean exist(char[][] board, String word) {
-        
+    public boolean exist(char[][] board, String word) {  
+
         for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < board[0].length; j++) {
-                if(board[i][j] == word.charAt(0)) {
-                    if(checkWord(word, board, i, j, 0)) {
-                        return true;
-                    }
+            for(int j = 0; j < board[i].length; j++) {
+                if(board[i][j] == word.charAt(0))
+                if(checkWord(i,j, board, word, 0)) {
+                    return true;
                 }
             }
-        } 
+        }
         return false;
-    }
+    }    
     
-    public boolean checkWord(String word, char[][] board, int row, int col, int ind) {
-        boolean checker = false;
-        
-        if( row < 0 || col < 0 || row >= board.length || col >= board[0].length || board[row][col] != word.charAt(ind)) {
+    private boolean checkWord(int i, int j, char[][] board, String word, int index) {
+            
+        if(i < 0 || i >= board.length || j < 0 || j >= board[i].length || index >= word.length() || board[i][j] != word.charAt(index)) {
             return false;
         }
         
+        if(index == word.length() -1) return true;
         
-        if(ind == word.length() - 1) {
-            return true;
-        }
+        char t = board[i][j];
+        board[i][j] = ' ';
         
-        char temp = board[row][col];
-        board[row][col] = ' ';
+        boolean checker = false;
+        checker = checkWord(i, j-1, board, word, index + 1) || checkWord(i, j+1, board, word, index + 1) ||
+            checkWord(i+1, j, board, word, index + 1) || checkWord(i-1, j, board, word, index + 1);
         
-        checker = checkWord(word, board, row - 1, col, ind+1) || checkWord(word, board, row + 1, col, ind+1) ||
-                 checkWord(word, board, row, col - 1, ind+1) ||  checkWord(word, board, row, col + 1, ind+1);
         
-        board[row][col] = temp;
+        board[i][j] = t;
+        
+        
+        
         return checker;
     }
-    
 }
