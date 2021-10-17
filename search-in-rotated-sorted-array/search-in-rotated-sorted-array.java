48d921fea1522b333ef051f318d816361d18fbd7
skim-93 <68123310+skim-93@users.noreply.github.com>
2021-10-16 18:05:47 -0700

    Time: 0 ms (100.00%), Space: 38.2 MB (86.50%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,18 +1,13 @@
 class Solution {
     public int search(int[] nums, int target) {
-        //int start, end 
         int start = 0;
         int end = nums.length -1;
         
         //while(start <= end)
         while(start <= end) {
+            int mid = (start + end) / 2;
             
-            int mid = (start + end) /2 ;
-            System.out.println(mid);
-            
-            if(nums[mid] == target) {
-                return mid;
-            } 
+            if(nums[mid] == target) return mid;
             
             if(nums[mid] >= nums[start]) {
                 if(target >= nums[start] && target < nums[mid]) {
@@ -27,21 +22,8 @@ class Solution {
                     end = mid - 1;
                 }
             }
-            
         }
-        
-        //if mid == target => return mid
-        
-        //if start > target && mid > target => start = mid + 1
-        
-        //if target < mid && target > end => end = mid - 1;
-        
-        //not found return -1;
         
         return -1;
     }
 }
-
-//[ 4 5 1 2 3]
-
-//[1,2,3,4,5]
