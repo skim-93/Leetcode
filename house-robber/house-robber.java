254ba9274ab11c25d2cc7b9d7eff83728c44a640
skim-93 <68123310+skim-93@users.noreply.github.com>
2021-10-16 18:38:16 -0700

    Time: 0 ms (100.00%), Space: 38.5 MB (11.51%) - LeetHub

--- house-robber/house-robber.java
+++ house-robber/house-robber.java
@@ -6,14 +6,10 @@ class Solution {
             
             if(i == 0) dp[i] = nums[i];
             
-            else if(i == 1) dp[i] = Math.max(dp[i-1], nums[1]);
+            else if(i == 1) dp[i] = Math.max(dp[0], nums[1]);
             
             
             else dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
-        }
-        
-        for(int i : dp) {
-            System.out.println(i);
         }
         
         return dp[nums.length -1];
