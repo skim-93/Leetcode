937790efdb6be134e3da51385de5e336267dbe88
skim-93 <68123310+skim-93@users.noreply.github.com>
2021-10-18 22:23:48 -0700

    Time: 3 ms (50.86%), Space: 64.1 MB (17.92%) - LeetHub

--- jump-game/jump-game.java
+++ jump-game/jump-game.java
@@ -1,13 +1,16 @@
 class Solution {
     public boolean canJump(int[] nums) {
-	if(nums.length == 1) return true;
-	int furthestPos = 0;
-
-	for(int i = 0; i < nums.length - 1; i++){
-		if(nums[i] == 0 && i == furthestPos) return false;
-		furthestPos = Math.max(furthestPos, i + nums[i]);
-		if(furthestPos >= nums.length - 1) return true;
-	}
-	return false;
-}
+        if(nums.length == 1 )return true;
+        int furthest = 0;
+        
+        for(int i = 0; i < nums.length - 1; i++) {
+            if(nums[i] == 0 && i == furthest) return false;
+            furthest = Math.max(furthest, i + nums[i]);
+            if(furthest >= nums.length - 1) return true;
+        }
+    
+    
+        return false;
+    
+    }
 }
