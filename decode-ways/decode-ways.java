2f7499a3ec08accd79c55d93ee1f19dfa09812ce
skim-93 <68123310+skim-93@users.noreply.github.com>
2021-10-18 22:18:49 -0700

    Time: 2 ms (46.42%), Space: 38.6 MB (46.82%) - LeetHub

--- decode-ways/decode-ways.java
+++ decode-ways/decode-ways.java
@@ -1,50 +1,30 @@
 class Solution {
     public int numDecodings(String s) {
-        
-        Map<Character, String> map = new HashMap();
-        map.put('A', "1");
-        map.put('B', "2");
-        map.put('C', "3");
-        map.put('D', "4");
-        map.put('E', "5");
-        map.put('F', "6");
-        map.put('G', "7");
-        map.put('H', "8");
-        map.put('I', "9");
-        map.put('J', "10");
-        map.put('K', "11");
-        map.put('L', "12");
-        map.put('M', "13");
-        map.put('N', "14");
-        map.put('O', "15");
-        map.put('P', "16");
-        map.put('Q', "17");
-        map.put('R', "18");
-        map.put('S', "19");
-        map.put('T', "20");
-        map.put('U', "21");
-        map.put('V', "22");
-        map.put('W', "23");
-        map.put('X', "24");
-        map.put('Y', "25");
-        map.put('Z', "26");
-        
-        int[] dp = new int[s.length()];
-        
-        if(s.charAt(0) == '0') return 0;
-        
-        dp[0] = 1;
-        
-        for(int i = 1; i < s.length(); i++) {
-            
-            if(s.charAt(i) != '0') dp[i] = dp[i-1];
-            
-            if(Integer.valueOf(s.substring(i-1, i+1)) <=26 && Integer.valueOf(s.substring(i-1,i+1)) >= 10) {
-                if(i-2 >= 0) dp[i] += dp[i-2];
-                else dp[i] += 1;
-            }
-        }
-        
-        return dp[s.length() - 1];
+    if (s == null || s.isEmpty()) 
+    	return 0;
+    if (s.charAt(0) == '0')
+    	return 0;
+    int[] f = new int[s.length() + 1];
+    f[0] = 1; f[1] = 1;
+    for (int i = 1; i < s.length(); i++) {
+    	if (s.charAt(i) == '0') {    //'0'
+    		if (s.charAt(i - 1) > '2' || s.charAt(i - 1) == '0')
+    			return 0; //invalid string
+    		else {
+    			f[i+1] = f[i - 1];
+    		}
+    	} else if (s.charAt(i) > '0' && s.charAt(i) < '7') {  //'1' - '6'
+    		if (s.charAt(i-1) > '0' && s.charAt(i-1) < '3') {
+    			f[i+1] = f[i] + f[i-1];
+    		} else 
+    			f[i+1] = f[i];
+    	} else {   //'7' - '9'
+    		if (s.charAt(i-1) == '1') 
+    			f[i+1] = f[i] + f[i-1];
+    		else 
+    			f[i+1] = f[i];
+    	}
+    }
+    return f[s.length()];
     }
 }
