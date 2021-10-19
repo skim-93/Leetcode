87758e7a46ba96b4faa1818564fb25761e578952
skim-93 <68123310+skim-93@users.noreply.github.com>
2021-10-18 22:39:44 -0700

    Time: 2 ms (92.00%), Space: 47.8 MB (40.04%) - LeetHub

--- number-of-islands/number-of-islands.java
+++ number-of-islands/number-of-islands.java
@@ -1,8 +1,6 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        
         int count = 0;
-        
         for(int i = 0; i < grid.length; i++) {
             for(int j = 0; j < grid[i].length; j++) {
                 if(grid[i][j] == '1') {
@@ -20,9 +18,9 @@ class Solution {
         
         grid[i][j] = '0';
         dfs(grid, i+1, j);
-        dfs(grid, i, j+1);
-        dfs(grid, i, j-1);
-        dfs(grid, i-1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
         return 1;
         
     }
